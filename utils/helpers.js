84cca28f5fbdea5e18260e7d748d7394bd607774
patricktheodore <patricktheodoresara@gmail.com>
2021-12-01 21:50:16 +0800

    homepage style fixed

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,4 +4,5 @@ module.exports = {
         return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
     },
     format_plural: (word, amount) => amount !== 1 ? `${word}s` : word,
+    
 };
