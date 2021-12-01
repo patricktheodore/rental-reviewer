e29dcebaea1d8591f519a1042e90cea2b4237e4a
patricktheodore <patricktheodoresara@gmail.com>
2021-12-01 21:34:28 +0800

    reviews styled

--- public/js/review.js
+++ public/js/review.js
@@ -2,10 +2,22 @@ const newReviewHandler = async (event) => {
     event.preventDefault();
   
     const property_id = event.target.getAttribute('data-id');
-    const title = document.querySelector('#reviewTitle').value.trim();
+    const title = document.querySelector('#review-title').value.trim();
     const rating = $('input[name=reviewRating]:checked').val();
-    const description = document.querySelector('#reviewDescription').value.trim();
+    const description = document.querySelector('#review-desc').value.trim();
   
+    if (!title) {
+      alert('Please Enter a Title for your Review')
+    };
+
+    if (!rating) {
+      alert('Please enter a rating for your review')
+    };
+
+    if (!description) {
+      alert('Please give more details about your review.')
+    }
+    
     if (title && rating && description) {
       const response = await fetch(`/api/reviews`, {
         method: 'POST',
@@ -18,7 +30,7 @@ const newReviewHandler = async (event) => {
       if (response.ok) {
         document.location.reload();
       } else {
-        alert('Failed to create post');
+        alert('It seems you have already reviewed this property. You can view and edit your reviews from your dashboard.');
       }
     }
   };
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 module.exports = {
     format_date: date => {
         date = new Date(date);
-        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
     },
     format_plural: (word, amount) => amount !== 1 ? `${word}s` : word,
 };
