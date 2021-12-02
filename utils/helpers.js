9c3b5c739c919977ddc3d33e45a35769cecb4f64
patricktheodore <patricktheodoresara@gmail.com>
2021-12-02 08:31:03 +0800

    star icon for rating

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,5 +4,13 @@ module.exports = {
         return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
     },
     format_plural: (word, amount) => amount !== 1 ? `${word}s` : word,
-    
+
+    // rating_average: rating => {
+    //     var sum = 0;
+    //     for( var i = 0; i < elmt.length; i++ ){
+    //         sum += parseInt( elmt[i], 10 ); //don't forget to add the base
+    //     }
+        
+    //     var avg = sum/elmt.length;
+    // } 
 };
